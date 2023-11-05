1d230bc7458b48acad1d0be76be474459db0b4af
LORIS FABRIZIO MARIO VITALE S317264 <s317264@studenti.polito.it>
2023-11-05 16:30:56 +0100

    modified auth

--- code/server/routes/auth/auth.js
+++ code/server/routes/auth/auth.js
@@ -1,54 +1,43 @@
-
-const express = require('express');
-const router = express.Router();
 const passport = require('passport');
 
-
-const isLoggedIn = (req, res, next) => {
+export const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   return res.status(401).json({ error: 'Not authorized' });
 };
 
-// POST /api/sessions
-router.post('/api/sessions', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-        if (err)
-          return next(err);
-        if (!user) {
-          // display wrong login messages
-          return res.status(401).send(info);
-        }
-        // success, perform the login
-        req.login(user, (err) => {
-          if (err)
-            return next(err);
-    
-          // req.user contains the authenticated user, we send all the user info back
-          return res.status(201).json(req.user);
-        });
-      })(req, res, next);
-});
+export const login = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+    if (!user) {
+      // display wrong login messages
+      return res.status(401).send(info);
+    }
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err)
+        return next(err);
 
-// GET /api/sessions/current
-router.get('/current', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.status(200).json(req.user);
-      }
-      else
-        res.status(401).json({ error: 'Not authenticated' });
-});
+      // req.user contains the authenticated user, we send all the user info back
+      return res.status(201).json(req.user);
+    });
+  })(req, res, next);
+};
 
-// DELETE /api/sessions/current
-router.delete('/api/sessions/current', isLoggedIn, (req, res) => {
-    req.logout(() => {
-        res.sendStatus(204);
-      });
-});
+export const getCurrentSession = (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else {
+    res.status(401).json({ error: 'Not authenticated' });
+  }
+};
 
-module.exports = router;
+export const logout = (req, res) => {
+  req.logout(() => {
+    res.sendStatus(204);
+  });
+};
 
 
-
-  
