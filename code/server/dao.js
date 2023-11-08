8d25cad590b2b9763ff2043842eebda72d257378
Andrea Torredimare <andrea@torredimare.it>
2023-11-08 14:39:56 +0100

    Dao function to get thesis proposals (student)

--- code/server/dao.js
+++ code/server/dao.js
@@ -83,3 +83,39 @@ exports.getUser = (email, password) =>
  */
 
 
+// STUDENT SECTION
+exports.getThesisProposals = (degCode) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * from thesis_proposals WHERE cds=?' //a student can get thesis proposals only from his study course
+    db.all(
+      sql,
+      [degCode],
+      (err,rows) => {
+        if(err) {
+          reject(err)
+        } else if(rows.length == 0) {
+          resolve({error: `No thesis proposals found for study course ${degCode}`});
+        } else {
+          const proposals = rows.map( (row) => (
+            {
+              id: row.id,
+              title: row.title,
+              supervisor: row.supervisor,
+              cosupervisors: row.cosupervisors,
+              keywords: row.keywords,
+              type: row.type,
+              groups: row.groups,
+              description: row.description,
+              requirements: row.requirements,
+              notes: row.notes,
+              expiration: row.expiration,
+              level: row.level,
+              cds: row.cds
+            }
+          ));
+          resolve(proposals);
+        }
+      }
+    )
+  });
+}
