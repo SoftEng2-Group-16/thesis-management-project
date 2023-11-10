e0c7d06ab3dfe74a0dfea6e74461baa4d973b9fa
Andrea Torredimare <andrea@torredimare.it>
2023-11-10 12:46:43 +0100

    Fix: parsing correctly string fields into arrays

--- code/server/dao.js
+++ code/server/dao.js
@@ -3,33 +3,36 @@ const db = require('./db');
 // STUDENT SECTION
 exports.getThesisProposals = (degCode) => {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT * from thesis_proposals WHERE cds=?' //a student can get thesis proposals only from his study course
+    const sql = 'SELECT * from thesis_proposals'
     db.all(
       sql,
-      [degCode],
+      [],
       (err,rows) => {
         if(err) {
           reject(err)
         } else if(rows.length == 0) {
           resolve({error: `No thesis proposals found for study course ${degCode}`});
         } else {
-          const proposals = rows.map( (row) => (
-            {
-              id: row.id,
-              title: row.title,
-              supervisor: row.supervisor,
-              cosupervisors: row.cosupervisors,
-              keywords: row.keywords,
-              type: row.type,
-              groups: row.groups,
-              description: row.description,
-              requirements: row.requirements,
-              notes: row.notes,
-              expiration: row.expiration,
-              level: row.level,
-              cds: row.cds
-            }
-          ));
+          console.log(rows);
+          const proposals = rows
+            .filter( r => r.cds.match(degCode) != null)
+            .map( (row) => (
+              {
+                id: row.id,
+                title: row.title,
+                supervisor: row.supervisor,
+                cosupervisors: row.cosupervisors.split('-'),
+                keywords: row.keywords,
+                type: row.type,
+                groups: row.groups.split('-'),
+                description: row.description,
+                requirements: row.requirements,
+                notes: row.notes,
+                expiration: row.expiration,
+                level: row.level,
+                cds: row.cds.split(','),
+              }
+            ));
           resolve(proposals);
         }
       }
