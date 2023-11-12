9667501de18618dd2a1fd9532b4665709c18b555
lfmvit <80250410+lfmvit@users.noreply.github.com>
2023-11-12 18:47:54 +0100

    updated clock using react bootstrap

--- code/client/src/components/Clock.jsx
+++ code/client/src/components/Clock.jsx
@@ -1,26 +1,62 @@
-// Clock.js
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
+import { Modal, Button } from 'react-bootstrap';
 
-function Clock({ onDateChange }) {
-  const [currentDate, setCurrentDate] = useState(dayjs().format('YYYY-MM-DD'));
+function Clock({ onDateChange, onConfirm }) {
+  const [currentDate, setCurrentDate] = useState(dayjs().format('DD-MM-YYYY'));
+  const [showModal, setShowModal] = useState(false);
 
   const handleChange = (event) => {
-    const newDate = event.target.value;
-    setCurrentDate(newDate);
+    setCurrentDate(event.target.value);
+  };
 
+  const handleConfirm = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleConfirmModal = () => {
+    setShowModal(false);
     if (onDateChange) {
-      onDateChange(newDate);
+      onDateChange(currentDate);
+      if (onConfirm) {
+        onConfirm();
+      }
     }
   };
 
   return (
-    <input
-      type="date"
-      value={currentDate}
-      onChange={handleChange}
-      style={{ cursor: 'pointer' }}
-    />
+    <div>
+      <input
+        type="date"
+        value={currentDate}
+        onChange={handleChange}
+        style={{ cursor: 'pointer' }}
+      />
+      <Button variant="primary" onClick={handleConfirm}>
+        Change System Time
+      </Button>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {`Do you want to change the system time to ${currentDate}?`}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleConfirmModal}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
 
