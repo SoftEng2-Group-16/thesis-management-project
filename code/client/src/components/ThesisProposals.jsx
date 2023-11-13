fc070ac2df4363e2b58feda7e443aa5a83a2a429
ENDRI SEFA S319103 <s319103@studenti.polito.it>
2023-11-13 14:49:12 +0100

    Filter part

--- code/client/src/components/ThesisProposals.jsx
+++ code/client/src/components/ThesisProposals.jsx
@@ -9,8 +9,9 @@ import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Select from 'react-select'
+import Select from 'react-select';
 import { Col, Row } from 'react-bootstrap';
+import makeAnimated from 'react-select/animated';
 
 /*
 here we can implement the home, the home shows the thesis;
@@ -28,11 +29,27 @@ function ThesisProposals(props) {
   const [thesis, setThesis] = useState([]);
   const [modalShow, setModalShow] = useState(false);
   const [filter, setFilter] = useState();
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
+  const [options, setOptions] = useState([]);
+  const [selections, setSelections] = useState([]);
+  const [title, setTitle] = useState([])
+  const [supervisor, setSupervisor] = useState([])
+  const [cosupervisors, setCosupervisors] = useState([])
+  const [keywords, setKeywords] = useState([])
+  const [type, setType] = useState([])
+  const [groups, setGroups] = useState([])
+  const [level, setLevel] = useState([])
+  const [cds, setCds] = useState([])
+
+  const [version, setVersion] = useState(0);
+
+  //function to reset the selector
+  function handleReset() {
+    setVersion(version + 1);
+  }
+
+
+  //constant to animate the filter selector
+  const animatedComponents = makeAnimated();
 
 
   useEffect(() => {
@@ -41,10 +58,10 @@ function ThesisProposals(props) {
       id: "1",
       title: "Thesis 1", 
       supervisors:"1001", 
-      cosupervisors:"1001, 10101", 
-      keywords: "AI", 
+      cosupervisors:["10001", "10101"], 
+      keywords: ["AI"], 
       type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      group: ["AI Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
@@ -54,25 +71,25 @@ function ThesisProposals(props) {
     },{ 
       id: "2",
       title: "Thesis 2",
-      supervisors:"1001",  
-      cosupervisors:"1001, 10101", 
-      keywords: "AI", 
+      supervisors:"1002",  
+      cosupervisors:["10002", "10103"], 
+      keywords: ["AI","Data Science"], 
       type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      group: ["Data Research Group", "Medical Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
       expiration: "20-11-24", 
-      level: "bachelor", 
+      level: "Master", 
       cds: "LT-2" 
     },{ 
       id: "3",
       title: "Thesis 3", 
-      supervisors:"1001", 
-      cosupervisors:"1001, 10101", 
-      keywords: "Data Science", 
-      type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      supervisors:"1003", 
+      cosupervisors:["10002", "10101"], 
+      keywords: ["Data Science"], 
+      type: "Research Thesis", 
+      group: ["AI Research Group", "Medical Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
@@ -85,10 +102,10 @@ function ThesisProposals(props) {
       id: "1",
       title: "Thesis 1", 
       supervisors:"1001", 
-      cosupervisors:"1001, 10101", 
-      keywords: "AI", 
+      cosupervisors:["10001", "10101"], 
+      keywords: ["AI"], 
       type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      group: ["AI Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
@@ -98,25 +115,25 @@ function ThesisProposals(props) {
     },{ 
       id: "2",
       title: "Thesis 2",
-      supervisors:"1001",  
-      cosupervisors:"1001, 10101", 
-      keywords: "AI", 
+      supervisors:"1002",  
+      cosupervisors:["10002", "10103"], 
+      keywords: ["AI","Data Science"], 
       type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      group: ["Data Research Group", "Medical Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
       expiration: "20-11-24", 
-      level: "bachelor", 
+      level: "Master", 
       cds: "LT-2" 
     },{ 
       id: "3",
       title: "Thesis 3", 
-      supervisors:"1001", 
-      cosupervisors:"1001, 10101", 
-      keywords: "Data Science", 
-      type: "Company Thesis", 
-      group: "AI Research Group, Medical Research Group", 
+      supervisors:"1003", 
+      cosupervisors:["10002", "10101"], 
+      keywords: ["Data Science"], 
+      type: "Research Thesis", 
+      group: ["AI Research Group", "Medical Research Group"], 
       description: "Develop AI-powered healthcare solutions for diagnosing diseases.", 
       requirements: "Machine Learning, Medical Science, Data Analysis", 
       notes: "This project focuses on leveraging AI for healthcare advancements.", 
@@ -124,6 +141,21 @@ function ThesisProposals(props) {
       level: "bachelor", 
       cds: "LT-2" 
     }])
+
+    setFilter("title")
+
+    setTitle(["Thesis 1","Thesis 2","Thesis 3"])
+    setSupervisor(["1001","1002","1002"])
+    setCosupervisors(["10001", "10002", "10103", "10101"])
+    setKeywords(["AI", "Data Science"])
+    setType(["Company Thesis", "Research Thesis"])
+    setLevel(["Bachelor", "Master"])
+    setGroups(["Data Research Group", "Medical Research Group", "AI Research Group"])
+    setCds(["LT-1", "LT-2", "LT-3"])
+    setOptions([
+      { value: "Thesis 1", label: "Thesis 1"},
+      { value: "Thesis 2", label: "Thesis 2"},
+      { value: "Thesis 3", label: "Thesis 3"}])
     
     /*this part will be used later and changed in order to set up the thesis array depending on the user type
 
@@ -145,31 +177,67 @@ function ThesisProposals(props) {
     }
     checkAuth();*/
   }, []);
- 
+  
+
+  function changeParameter(parameter){
+    setFilter(parameter)
+    setSelections([])
+    setOptions(options)
+    handleReset()
+    console.log(parameter)
+    /*
+    if(parameter = )
+    let array = []
+    selection.forEach(item => {
+        array.push(item.value)
+    })
+    setSelections(array)*/
+  }
 
   function filtering(){
     var listThesis = [...Allthesis]
-    if(filter == "") {setThesis(listThesis)
+    console.log(options)
+    if(filter.lenght == 0) {setThesis(listThesis)
     }else{
+
     listThesis = listThesis.filter((item) => {
+
       if(filter == item.keywords) return item
     });
     console.log(listThesis)
     setThesis(listThesis)}
   }
 
+  function changeSelection(selection){
+    if(selection.lenght == 0){
+      setOptions([])
+    }else{
+      let array = []
+      selection.forEach(item => {
+          array.push(item.value)
+      })
+      setSelections(array)
+    }
+  }
+
+
 
   return (
     <>
     {props.loggedIn && props.user.role === "student" ? (
       <div style={{ marginTop: '10px' }}> 
-                <Form className="d-flex" onChange={(event) => {setFilter(event.target.value)}}>
-                <Form.Select aria-label="Default select example" style={{ lenght: '10px' }}>
+                <Form className="d-flex" >
+                <Form.Select aria-label="Default select example" className="selector"onChange={(event) => {changeParameter(event.target.value)}}>
                   <option value="title">Title</option>
-                  <option value="group">Group</option>
-                  <option value="keyword">Keyword</option>
+                  <option value="supervisor">Supervisor</option>
+                  <option value="cosupervisors">Cosupervisors</option>
+                  <option value="keywords">Keywords</option>
+                  <option value="Type">Type</option>
+                  <option value="groups">Groups</option>
+                  <option value="level">Level</option>
+                  <option value="cds">Course of study</option>
                 </Form.Select>
-                <Select options={options} />
+                <Select options={options} key={version} className="parameters"  closeMenuOnSelect={true} components={animatedComponents} isMulti onChange={(event) => changeSelection(event)}/>
             <Button variant="outline-success" onClick={()=>filtering()}>Search</Button>
           </Form>
 
