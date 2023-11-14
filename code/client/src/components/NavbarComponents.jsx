4f86748235910b611f0cbdf745ea9f1956d8383e
Andrea Torredimare <andrea@torredimare.it>
2023-11-14 22:00:36 +0100

    Changed layout of the navbar (date on the left under app name)

--- code/client/src/components/NavbarComponents.jsx
+++ code/client/src/components/NavbarComponents.jsx
@@ -15,26 +15,29 @@ function NavHeader(props) {
   };
 
   return (
-  <Navbar className="d-flex justify-content-around" bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand  className='navbar-brand'>
-      Shitty Thesis Management
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href='#'>Thesis</Nav.Link>
-      </Nav>
-      {props.loggedIn ? <>
-      <Navbar.Text>
-      <Clock onDateChange={props.onDateChange} />
-      <img src={User} style={{ width: '45px', height: 'auto', fill: 'white' }} alt="User:" />
-        <span className="text-light me-3 username">{props.user.role}</span>
-      </Navbar.Text>
-        <LogoutButton logout={props.handleLogout} />
+    <Navbar className="d-flex justify-content-around" bg="primary" variant="dark">
+      <Container>
+        <div className='navbar-brand'>
+          <Navbar.Brand  >
+            Shitty Thesis Management
+          </Navbar.Brand>
+          {props.loggedIn ? <Clock onDateChange={props.onDateChange} /> : null}
+        </div>
+
+        <Nav className="me-auto">
+          <Nav.Link href='#'>Thesis</Nav.Link>
+        </Nav>
+        {props.loggedIn ? <>
+          <Navbar.Text>
+            <img src={User} style={{ width: '45px', height: 'auto', fill: 'white' }} alt="User:" />
+            <span className="text-light me-3 username">{props.user.role}</span>
+          </Navbar.Text>
+          <LogoutButton logout={props.handleLogout} />
         </> :
-        <Link to='/login'className='btn btn-outline-light'>Login</Link>
-         }
-         </Container>
-  </Navbar>
+          <Link to='/login' className='btn btn-outline-light'>Login</Link>
+        }
+      </Container>
+    </Navbar>
   );
 }
 
