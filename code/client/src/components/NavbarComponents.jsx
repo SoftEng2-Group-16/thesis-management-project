93f7bd6fa2da98619e2f71450b6767f5b1f1164c
Andrea Torredimare <andrea@torredimare.it>
2023-12-12 17:12:53 +0100

    Modified cursor when hovering over clock icon

--- code/client/src/components/NavbarComponents.jsx
+++ code/client/src/components/NavbarComponents.jsx
@@ -31,9 +31,11 @@ function NavHeader(props) {
             if (props.loggedIn)
               setShowClock(!showClock);
           }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
-            </svg>
+            <div className='clock-icon'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+              </svg>
+            </div>
           </span>
           {props.loggedIn && showClock && (
             <Clock
@@ -86,7 +88,7 @@ function NavHeader(props) {
                 </Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link as={Link} to="/applications" id='applications' className="nav-link"  eventKey="/applications" onClick={() => { props.setMessage(''); navigate('/applications') }}>
+                <Nav.Link as={Link} to="/applications" id='applications' className="nav-link" eventKey="/applications" onClick={() => { props.setMessage(''); navigate('/applications') }}>
                   Applications
                 </Nav.Link>
               </Nav.Item>
